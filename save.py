b249cbaadd037a0bdfcd78803a27500330c8910b
17mcpc14 <36224815+17mcpc14@users.noreply.github.com>
2019-07-25 16:51:06 +0530

    Create save.py

---
+++ save.py
@@ -0,0 +1,59 @@
+from __future__ import print_function
+from keras.models import Sequential, Model
+from keras.layers import Dense, Activation, Dropout
+from keras.layers import LSTM, Input, Bidirectional
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+from keras.metrics import categorical_accuracy
+import spacy
+import numpy as np
+import random
+import sys
+import os
+import time
+import codecs
+import collections
+from six.moves import cPickle
+from keras.callbacks import ModelCheckpoint
+import io
+import tensorflow as tf
+
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
+    """
+    Freezes the state of a session into a pruned computation graph.
+
+    Creates a new computation graph where variable nodes are replaced by
+    constants taking their current value in the session. The new graph will be
+    pruned so subgraphs that are not necessary to compute the requested
+    outputs are removed.
+    @param session The TensorFlow session to be frozen.
+    @param keep_var_names A list of variable names that should not be frozen,
+                          or None to freeze all the variables in the graph.
+    @param output_names Names of the relevant graph outputs.
+    @param clear_devices Remove the device directives from the graph for better portability.
+    @return The frozen graph definition.
+    """
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ""
+        frozen_graph = tf.graph_util.convert_variables_to_constants(
+            session, input_graph_def, output_names, freeze_var_names)
+        return frozen_graph
+
+from keras.models import load_model
+# load the model
+print("loading model...")
+model = load_model(save_dir + "/" + 'my_model_generate_sentences.h5')
+
+from keras import backend as K
+
+frozen_graph = freeze_session(K.get_session(),
+                              output_names=[out.op.name for out in model.outputs])
+
+tf.train.write_graph(frozen_graph, save_dir, "my_model.pb", as_text=False)
